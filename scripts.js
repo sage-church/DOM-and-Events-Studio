dfdf33b6394644e76291093d3dc29287a5023159
sage-church <sagin.churchman77@gmail.com>
2022-08-10 22:58:30 -0500

    added features

--- scripts.js
+++ scripts.js
@@ -1,2 +1,52 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+
+    let takeoffButton = document.getElementById("takeoff");
+    let landButton = document.getElementById("landing");
+    let abortButton = document.getElementById("missionAbort");
+    let upButton = document.getElementById("up");
+    let downButton = document.getElementById("down");
+    let rightButton = document.getElementById("right");
+    let leftButton = document.getElementById("left");
+    let flightStatus = document.getElementById("flightStatus");
+    let shuttleBackground = document.getElementById("shuttleBackground");
+    let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+
+    takeoffButton.addEventListener("click", function() {
+        if (confirm("Confirm that the shuttle is ready for takeoff.")) {
+            flightStatus.innerHTML = "Shuttle in flight.";
+            shuttleBackground.style.backgroundColor = "blue";
+            spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) + 10000;
+        }
+    });
+
+    landButton.addEventListener("click", function() {
+        alert("The shuttle is landing. Landing gear engaged.");
+        flightStatus.innerHTML = "The shuttle has landed.";
+        shuttleBackground.style.backgroundColor = "green";
+        spaceShuttleHeight.innerHTML = 0;
+    });
+
+    abortButton.addEventListener("click", function() {
+        if (confirm("Confirm that you want to abort the mission.")) {
+            flightStatus.innerHTML = "Mission aborted.";
+            shuttleBackground.style.backgroundColor = "green";
+            spaceShuttleHeight.innerHTML = 0;
+        }
+    });
+
+    upButton.addEventListener("click", function() {
+
+    });
+
+    downButton.addEventListener("click", function() {
+        
+    });
+
+    rightButton.addEventListener("click", function() {
+        
+    });
+
+    leftButton.addEventListener("click", function() {
+        
+    });
+});
